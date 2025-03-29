13f96053adace600af62b033aa2c6d8376ebeb00
Sterryn <sterryn261@gmail.com>
2025-03-29 16:34:04 +0700

    Title added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import Header from './components/Header.vue';
 </script>
 
 <template>
+  <Header />
 </template>
 
 <style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <a href="/" class="title">
+      codemoe
+    </a>
+  </header>
+</template>
+
+<style scoped lang="scss">
+header {
+  position: fixed;
+  width: 100vw;
+  height: 3.5em;
+  background: #000;
+
+  .title {
+    position: absolute;
+    margin-top: 0.25em;
+    margin-left: 2.5em;
+
+    text-decoration: none;
+
+    font-size: 150%;
+    font-weight: bold;
+
+    color: #fff;
+  }
+}
+</style>
