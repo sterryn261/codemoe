65ac5612b02810a9fc37d1e5a6c3cbe4a0e998f6
Sterryn <sterryn261@gmail.com>
2025-03-29 16:40:22 +0700

    List and sidebar file prepared

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
 import Header from './components/Header.vue';
+import List from './components/List.vue';
+import Sidebar from './components/Sidebar.vue';
 </script>
 
 <template>
   <Header />
+  <div class="content">
+    <List />
+    <Sidebar />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss"></style>
