64caa0e011dc4ae467490a6aea37a1c6af3be435
Sterryn <sterryn261@gmail.com>
2025-03-30 14:56:33 +0700

    Removed subheader, moved sidebar and styled it.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,57 @@
 <script setup lang="ts">
 import Header from './components/Header.vue';
-import Subheader from './components/Subheader.vue';
 import List from './components/List/List.vue';
-import Sidebar from './components/Sidebar.vue';
 </script>
 
 <template>
   <Header />
-  <Subheader />
   <div class="content">
     <List />
-    <Sidebar />
+    <div class="sidebar">
+      <div class="user">
+        <input type="text" placeholder="Enter here...">
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
 .content {
   position: relative;
-  margin-top: 6em;
+  margin-top: 5em;
+}
+
+.sidebar {
+  position: absolute;
+  top: 0;
+  right: 0;
+
+  width: 24vw;
+  height: 10em;
+  margin-right: 2em;
+
+  .user {
+    width: 100%;
+    padding: 1em;
+    border-radius: 0.5em;
+
+    background: var(--ds-bg);
+
+    input {
+      width: 100%;
+      padding: 0.5em;
+      border: none;
+      border-radius: 0.5em;
+
+      color: var(--dp-text);
+      background: var(--ds-bg);
+    }
+
+
+    input:focus {
+      outline: none;
+      background: var(--dt-bg);
+    }
+  }
 }
 </style>
