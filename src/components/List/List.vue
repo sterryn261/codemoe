369ba6cba4b121a34aceadc3909bff4858f3bb67
Sterryn <sterryn261@gmail.com>
2025-04-02 15:49:59 +0700

    Dynamic scrolling

--- src/components/List/List.vue
+++ src/components/List/List.vue
@@ -1,18 +1,31 @@
 <script setup lang="ts">
+import { computed, ref } from 'vue';
 import type { ContestType, SubmissionType } from '../../types';
 import Contest from './Contest.vue';
 
-defineProps<{ cData: ContestType[] }>();
+const props = defineProps<{ cData: ContestType[] }>();
+let render = ref<number>(5);
+
+window.onscroll = (e) => {
+  if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
+    render.value += 5;
+  }
+};
+
+let contests = computed(() => {
+  return props.cData.slice(0, render.value);
+})
+
 </script>
 
 <template>
   <div class="list">
-    <Contest :contest="contest" v-for="contest in cData" />
+    <Contest :contest="contest" v-for="contest in contests" />
   </div>
 </template>
 
 <style scoped lang="scss">
 .list {
-  width: 70vw;
+  width: 73vw;
 }
 </style>
